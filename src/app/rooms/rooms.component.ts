44768f9fbe2c225321327fc558fe4a81ad4d311c
Romashka <rroma13579@gmail.com>
2023-01-06 23:06:45 +0100

    second

---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'roma-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.css']
+})
+export class RoomsComponent {
+  hotelName = "Roma`s Hotel";
+}
