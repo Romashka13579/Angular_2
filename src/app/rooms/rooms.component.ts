6ff40f76c564156d5d397de65437d96390132c34
Romashka <rroma13579@gmail.com>
2023-01-06 23:08:05 +0100

    third

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class RoomsComponent {
   hotelName = "Roma`s Hotel";
+  numberOfRooms = "10";
+
+  hideRooms = false;
+  
+  funcName(){
+    this.hideRooms = !this.hideRooms;
+  }
 }
